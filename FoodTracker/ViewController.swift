ecc0c74c219cb339dc78c8a1139e9078d0de9216
daniel leahy <danleahy@gmail.com>
2019-07-03 11:33:11 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by daniel leahy on 03/07/2019.
+//  Copyright © 2019 ucd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
