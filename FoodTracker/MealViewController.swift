816fe3ae0fa24eaa680bdde0672d94ec16c1e3ae
daniel leahy <danleahy@gmail.com>
2019-07-08 16:48:04 +0100

    ViewController renamed to MealViewController

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     
-    @IBOutlet weak var mealNameLabel: UILabel!
+   
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -33,7 +33,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     internal func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+       
     }
     
     
