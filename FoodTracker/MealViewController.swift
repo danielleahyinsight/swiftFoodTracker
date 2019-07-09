ced2283b19733e4b2ffcdd34f9c7e470d6a6b99b
daniel leahy <danleahy@gmail.com>
2019-07-09 17:08:10 +0100

    cancel button now working, differentiating between modal and non modal presentation of the mealdetailview controller to decide what to do

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -13,8 +13,6 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    
-   
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -32,6 +30,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         // Handle the text fields  user input through delegate callbacks
         nameTextField.delegate = self
+        
+        // Set up views if editing an existing Meal
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
         
         //Enable the save button only if the text field has valid Meal name
         updateSaveButtonState()
@@ -107,7 +113,20 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on the style of presentation (modal or push presentation), this view
+        // controller needs to be dismissed in two different ways
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The Meal View Controller is not inside a navigation controller")
+        }
+        
+        
     }
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
